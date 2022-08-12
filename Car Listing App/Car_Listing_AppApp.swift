8c22fff66916852d6f786893e0265d5ee32332a5
Semih Cetin <semihcettin@gmail.com>
2022-08-12 19:56:09 +0300

    Initial Commit

---
+++ Car Listing App/Car_Listing_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Car_Listing_AppApp.swift
+//  Car Listing App
+//
+//  Created by Semih Cetin on 12.08.2022.
+//
+
+import SwiftUI
+
+@main
+struct Car_Listing_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Car Listing App/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Car Listing App
+//
+//  Created by Semih Cetin on 12.08.2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
